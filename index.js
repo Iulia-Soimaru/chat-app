f3fc2d26629643f18f8b424255e46685dc286690
Iulia Soimaru <yuliga4@gmail.com>
2015-05-17 16:32:06 -0700

    change route to html file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello World</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
