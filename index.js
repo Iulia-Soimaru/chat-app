aa20394a00ba11754bf1c198c7b57a782e6f6f64
Iulia Soimaru <iuliashoimaru@gmail.com>
2015-05-17 17:47:26 -0700

    display messages live in the browser

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
   console.log('a user connected');
   socket.on('disconnect', function(){
